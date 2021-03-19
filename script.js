5c57f101c86519d16d97b24b219a650a129696b2
root <diegoserafim1@gmail.com>
2021-03-18 21:40:28 -0300

    D2 criado a funçao para pegar a imagem selecionada no input e retorna-la no container img

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const areaDigitavel = document.getElementById('text-input');
-const fotoSelecionada = document.getElementById('input#meme-image');
+// const fotoSelecionada = document.getElementById('meme-image');
+// const memeImageContainer = document.getElementById('meme-image-container');
 const retornaTextoDigitado = document.getElementById('meme-text');
+const memeInsert = document.getElementById('meme-insert');
 // console.log(retornaTextoDigitado + 'oi');
 // console.log(fotoSelecionada + 'oi');
 function inputDeEntrada() {
   const areaDigit = areaDigitavel.value;
   retornaTextoDigitado.innerHTML = areaDigit;
 }
-function fotoSelecionada() {
-  const fotoSelec = = 
+
+function criaImagem() {
+  // https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader/readAsDataURL
+  const fotoRetonor = document.querySelector('img');
+  const file = document.querySelector('input[type=file]').files[0];
+  const reader = new FileReader();
+  reader.onloadend = function func() {
+    fotoRetonor.src = reader.result;
+  };
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    fotoRetonor.src = '';
+  }
 }
 window.onload = function inicio() {
   areaDigitavel.addEventListener('input', inputDeEntrada);
-  alert('Sem retorno');
+  memeInsert.addEventListener('input', criaImagem);
 };
