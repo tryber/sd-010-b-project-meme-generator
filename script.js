33316fd8dfa861f3d8be46e800cb0ff9f46b5201
root <diegoserafim1@gmail.com>
2021-03-20 17:50:05 -0300

    Criado a funçao para criar a borda e os botoes para escolher a  borda

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const areaDigitavel = document.getElementById('text-input');
-const fotoSelecionada = document.getElementById('#meme-image');
+const fotoSelecionada = document.getElementById('meme-image');
 const memeImageContainer = document.getElementById('meme-image-container');
 const retornaTextoDigitado = document.getElementById('meme-text');
 const memeInsert = document.getElementById('meme-insert');
 // console.log(retornaTextoDigitado + 'oi');
 // console.log(fotoSelecionada + 'oi');
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
 function inputDeEntrada() {
   const areaDigit = areaDigitavel.value;
   retornaTextoDigitado.innerHTML = areaDigit;
 }
 
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
 function criaBorda(e) {
   const elementoAlvo = e.target;
   const cssSuporte = window.getComputedStyle(elementoAlvo).getPropertyValue('border');
@@ -19,27 +19,9 @@ function criaBorda(e) {
 
 function botaoBorda() {
   const botao = document.querySelectorAll('button');
-  botao.forEach((elementoAlvo) => elementoAlvo.addEventListener('click', criaBorda));
-}
-
-function apagaElemento() {
-  if (document.getElementById(memeInsert)) {
-    document.getElementById(memeInsert).remove();
-  }
-}
-apagaElemento();
-
-// https://developer.mozilla.org/pt-BR/docs/Web/API/Node/cloneNode
-function criaMiniaturas(e) {
-  const mini = e.target;
-  const clona = mini.cloneNode(true);
-  const cloneFilho = document.getElementById('meme-image-container').appendChild(clona);
-  cloneFilho.id = memeInsert;
-}
-
-function retonrnaMini() {
-  const pegaMini = document.querySelectorAll('#samples img');
-  pegaMini.forEach((elementoAlvo) => elementoAlvo.addEventListener('click', criaMiniaturas));
+  botao.forEach((elementoAlvo) => {
+    elementoAlvo.addEventListener('click', criaBorda);
+  });
 }
 
 function criaImagem() {
@@ -47,7 +29,7 @@ function criaImagem() {
   const criaImg = document.createElement('img');
   const imgFilho = memeImageContainer.appendChild(criaImg);
   imgFilho.id = fotoSelecionada;
-  const fotoVisual = document.querySelector('img#meme-image');
+  const fotoVisual = document.querySelector('#meme-image');
   const carregarFoto = document.querySelector('#meme-insert[type=file]').files[0];
   const lerInput = new FileReader();
   lerInput.onloadend = function func() {
@@ -61,7 +43,6 @@ function criaImagem() {
 }
 
 window.onload = function inicio() {
-  retonrnaMini();
   botaoBorda();
   areaDigitavel.addEventListener('input', inputDeEntrada);
   memeInsert.addEventListener('input', criaImagem);
