c8cd6af33ec31467e7e0cbda388054c254b56d2f
root <diegoserafim1@gmail.com>
2021-03-20 19:40:07 -0300

    Retirando a variavel fotoSelecionada e colocando 'meme-image' na linha 28

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function criaImagem() {
   const criaImg = document.createElement('img');
   const imgFilho = memeImageContainer.appendChild(criaImg);
   imgFilho.id = fotoSelecionada;
-  const fotoVisual = document.querySelector(fotoSelecionada);
+  const fotoVisual = document.querySelector('#meme-image');
   const carregarFoto = document.querySelector('#meme-insert[type=file]').files[0];
   const lerInput = new FileReader();
   lerInput.onloadend = function func() {
