3de58a763691eba9ac620b71f075f8833eefeccd
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-23 08:50:47 -0300

    Adiciona o input file para upload das imagens e ajusta o texto inserido

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const getInputText = document.getElementById('text-input'); // captura o texto .value
 const getMemeText = document.getElementById('meme-text'); // captura o texto da div
+// capturar o arquivo inserido
+const getMemeImage = document.getElementById('meme-insert');
+
 
 function write() {
   getMemeText.innerText = getInputText.value;
@@ -8,6 +11,8 @@ function write() {
 function create() {
   getInputText.addEventListener('keyup', write);
 }
+
+
 
 window.onload = function () {
   create();
